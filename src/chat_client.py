1acefdbeccce888c20a42ad8e5be849c504603cd
Pedrok99 <pedrohlima20@gmail.com>
2022-09-22 22:00:42 -0300

    use select syscall to handle multiple connections

--- src/chat_client.py
+++ src/chat_client.py
@@ -1,11 +1,15 @@
+from time import sleep
 from models.tcp_client import Client
 
 chat_client = Client('localhost', 2222)
 
 chat_client.connect()
 
-chat_client.send('Hello, im a client')
+while True:
+  print('Sending message in 5 seconds')
+  sleep(5)
+  chat_client.send('Hi, im {}'.format(chat_client.socket.getsockname()))
+   
 
-print(chat_client.receive())
 
 chat_client.close()
--- src/chat_server.py
+++ src/chat_server.py
@@ -1,15 +1,23 @@
+from time import sleep
 from models.tcp_server import Server
+from select import select
+
+lastRoomId = 0
+
+rooms = {}
 
 chat = Server()
-
 chat.create()
-
 chat.listen()
 
-chat.accept_connection()
+while True:
+  readable_changes, _, _= select( chat.connections + [chat.socket], [] , [])
+  print('-' * 20)
+  for client in readable_changes:
+    if client is chat.socket:
+      chat.accept_connection()
+    else:
+      print('Recieved message from {}: {}'.format(client.getpeername(), chat.get_message(client)))
 
-print(chat.get_message(chat.connections[0][0]))
-
-chat.send_message(chat.connections[0][0], 'Hello, im a server')
-
+  sleep(2)
 chat.close()
